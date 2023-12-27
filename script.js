02dd7baf46d3d7a4ef611b0bc3b1199cdc582ec1
RandomThacker <randomthacker@gmail.com>
2023-12-27 18:25:57 +0530

    feat : added numpad "1"

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ document.onkeydown = (e) => {
         gameStart()
     }
 
+    else if (e.keyCode == 97) {
+        gameStart()
+     }
+
     //------------Up Key--------------
     else if (e.keyCode == 38) {
         onUp();
